765ad115977acffb5fbc458b8447c64579ea3199
PedroRdrigues <p.hrdrigues1@gmail.com>
2024-11-15 17:29:16 -0300

    Ultimo commit antes do envio

--- src/main/java/com/userprofile/user_profile_demo/repositories/UserRepository.java
+++ src/main/java/com/userprofile/user_profile_demo/repositories/UserRepository.java
@@ -3,5 +3,5 @@ package com.userprofile.user_profile_demo.repositories;
 import com.userprofile.user_profile_demo.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
 }
